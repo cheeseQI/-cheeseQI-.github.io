f39e77e8661f6678ee768545c6b47658dbabb86c
cheese7 <793975015@qq.com>
2024-05-31 23:24:15 -0400

    feat. add two additional exp

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -19,9 +19,6 @@ function App() {
       case 'about':
         return (
           <section id="about" className="mb-4">
-            <h2>
-              About Me
-            </h2>
             <div className="content show">
               <p>
                 Software Engineer with a Master's degree, specializing in full-stack development and cloud technologies. Experienced in Java, Python, and JavaScript frameworks with a track record of multiple internships and contributions to open-source projects.
@@ -34,25 +31,7 @@ function App() {
       case 'experiences':
         return (
           <section id="experiences" className="mb-4">
-            <h2>
-              Past Interesting Coding Experiences
-            </h2>
             <div className="content show">
-              <div className="project-details">
-                <h3>Game Made on Hackathon</h3>
-                <p>"Be The Detective" is an immersive first-player experience that transforms you into a sleuth on your mobile device.</p>
-                <p>Join our game server and dive into a world of mystery where your choices shape the narrative:</p>
-                <ul>
-                  <li><strong>Interactive Gameplay:</strong> Engage with a story that responds to your every move.</li>
-                  <li><strong>Dynamic Environment:</strong> Explore scenarios that adapt in real time to your investigative tactics.</li>
-                  <li><strong>Powered by AI:</strong> Utilizing OpenAI API, every case unfolds in a unique fashion, providing a fresh challenge each time.</li>
-                </ul>
-                <a href={`https://www.youtube.com/watch?v=${hackathonGameVideoId}`} target="_blank" rel="noopener noreferrer" className="video-thumbnail">
-                  <div style={{ backgroundImage: `url(${hackathonGameThumbnail})` }} className="video-thumbnail-image">
-                    <FontAwesomeIcon icon={faPlayCircle} className="video-play-icon" />
-                  </div>
-                </a>
-              </div>
               <div className="project-details">
                 <h3>E-commerce Platform Developed in Part Time</h3>
                 <p>Say goodbye to sticker shock and rediscover the joy of bargain hunting with the app I've helped develop.</p>
@@ -66,15 +45,49 @@ function App() {
                   </p>
                   <a href="https://www.lordofsavings.com/" target="_blank" rel="noopener noreferrer">LordOfSavings</a>
               </div>
+              <div className="project-details">
+                <h3>Project Integrated into Alibaba Container Registry</h3>
+                <p>Image acceleration function intergated by K8s and open source project DADI</p>
+                <ul>
+                  <li><strong>Accelerated Service Delivery:</strong> Experience dramatically reduced wait times thanks to the integration of remote accelerated images and the Kubernetes API.</li>
+                  <li><strong>Innovative Compression and Transfer Technologies:</strong> Explore ZFile, and peer-to-peer on-demand transfer technology, built to manage the demands of massive and super-large scale clusters.</li>
+                </ul>
+                <p>
+                  Official handbook:
+                  </p>
+                  <a href="https://www.alibabacloud.com/help/en/acr/user-guide/load-resources-of-a-container-image-on-demand#section-87k-i16-i5g" target="_blank" rel="noopener noreferrer">Enable image acceleration</a>
+              </div>
+              <div className="project-details">
+                <h3>Game Made on Hackathon</h3>
+                <p>"Be The Detective" is an immersive first-player experience that transforms you into a sleuth on your mobile device.</p>
+                <ul>
+                  <li><strong>Interactive Gameplay:</strong> Engage with a story that responds to your every move.</li>
+                  <li><strong>Dynamic Environment:</strong> Explore scenarios that adapt in real time to your investigative tactics.</li>
+                  <li><strong>Powered by AI:</strong> Utilizing OpenAI API, every case unfolds in a unique fashion, providing a fresh challenge each time.</li>
+                </ul>
+                <a href={`https://www.youtube.com/watch?v=${hackathonGameVideoId}`} target="_blank" rel="noopener noreferrer" className="video-thumbnail">
+                  <div style={{ backgroundImage: `url(${hackathonGameThumbnail})` }} className="video-thumbnail-image">
+                    <FontAwesomeIcon icon={faPlayCircle} className="video-play-icon" />
+                  </div>
+                </a>
+              </div>
+              <div className="project-details">
+                <h3>AI and Cloud Integration Project</h3>
+                <p>Provide no-coding platform for the industry AI/ML training</p>
+                <ul>
+                  <li><strong>Full-Stack Web Application Development:</strong> Crafted a responsive web interface using React and engineered a robust backend with Flask and Amazon S3 on AWS, </li>
+                  <li><strong>Machine Learning Integration:</strong> Deployed machine learning models via AWS SageMaker, enabling endpoint access that streamlined application integration and improved user features and insights.</li>
+                </ul>
+                <p>
+                  No demo link since it is not open to pulbic yet.
+                </p>
+              </div>
             </div>
           </section>
         );
       case 'opensource':
         return (
           <section id="opensource" className="mb-4">
-            <h2>
-              Open Source Projects that I recommend and have contributions
-            </h2>
             <div className="content show">
               <div className="project-details">
                 <h3>Langchain</h3>
